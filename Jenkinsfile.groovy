e0d7dfafae0639e89a8dd84887a4998a7cf9bc02
Naveen Harikoppa <217095533+Naveen-5288@users.noreply.github.com>
2025-07-19 14:12:54 +0530

    Create Jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,27 @@
+pipeline {
+    agent any
+
+    environment {
+        AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
+        AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
+        BUCKET_NAME = 'my-static-site-5288'
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git 'https://github.com/yourusername/static-site.git'
+            }
+        }
+
+        stage('Deploy to S3') {
+            steps {
+                sh '''
+                aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+                aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+                aws s3 sync . s3://my-static-site-5288 --delete
+                '''
+            }
+        }
+    }
+}
